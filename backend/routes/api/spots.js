1e0f3dacda1cba8dfb7216b91957e7882f5ad2b7
Keenly <keenlyhere@gmail.com>
2022-12-20 12:39:11 -0800

    add endpoints for get current user spots and get by spotId

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
-const { Spot, Review, SpotImage, sequelize } = require("../../db/models");
+const { Spot, Review, SpotImage, User, sequelize } = require("../../db/models");
 
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
-
 const router = express.Router();
 
+
+// GET /api/spots
 router.get("/", async (req, res, next) => {
     const allSpots = await Spot.findAll({
         include: [
@@ -75,7 +76,6 @@ router.get("/", async (req, res, next) => {
         spotsArray.push(spotData)
     })
 
-    // console.log(spotsArray)
 
     const allSpotsData = {
         Spots: spotsArray
@@ -83,6 +83,133 @@ router.get("/", async (req, res, next) => {
 
 
     return res.json(allSpotsData)
+})
+
+
+// GET /api/spots/current
+router.get("/current", restoreUser, async (req, res, next) => {
+    const { user } = req;
+
+    const spotsByUser = await Spot.findAll({
+        where: {
+            ownerId: user.id
+        },
+        include: [
+            {
+                model: Review,
+                attributes: ['stars'],
+            },
+            {
+                model: SpotImage,
+                attributes: ['url', 'preview'],
+            }
+        ],
+    })
+
+    const spotsArray = [];
+
+    spotsByUser.forEach(spot => {
+        let count = spot.Reviews.length;
+        let sum = 0;
+        spot = spot.toJSON();
+
+        console.log(spot)
+
+        spot.Reviews.forEach(review => {
+            sum += review.stars
+        })
+
+        let avg = sum / count;
+
+        if (isNaN(avg)) {
+            avg = "Has not been rated yet"
+        }
+
+        spot.avgRating = avg;
+
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                // console.log("preview = true")
+                spot.previewImage = image.url
+                // console.log(image.url)
+            } else {
+                spot.previewImage = "No image listed"
+            }
+        })
+
+        const spotData = {
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            avgRating: spot.avgRating,
+            previewImage: spot.previewImage
+        }
+
+        spotsArray.push(spotData)
+    })
+
+
+    const spotsByUserData = {
+        Spots: spotsArray
+    };
+
+
+    return res.json(spotsByUserData)
+})
+
+router.get("/:spotId", async (req, res, next) => {
+    const spotId = req.params.spotId;
+
+    let spotById = await Spot.findByPk(spotId);
+
+    spotById = spotById.toJSON();
+
+    const count = await Review.count({
+        where: {
+            spotId: spotId
+        }
+    });
+
+    spotById.numReviews = count;
+
+    const sum = await Review.sum('stars', {
+        where: {
+            spotId: spotId
+        }
+    })
+
+    const avg = sum / count;
+
+    if (isNaN(avg)) {
+        avg = "Has not been rated yet"
+    }
+
+    spotById.avgStarRating = avg;
+
+    spotById.SpotImages = await SpotImage.findAll({
+        where: {
+            spotId: spotId
+        },
+        attributes: ['id', 'url', 'preview']
+    })
+
+    spotById.Owner =  await User.findByPk(spotById.ownerId, {
+        attributes: {
+            exclude: ['username']
+        }
+    })
+
+    res.json(spotById);
 })
 
 module.exports = router;
