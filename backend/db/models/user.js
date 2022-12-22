f33f4806bc79bb82a89203ead5527ffa396ff31b
Keenly <keenlyhere@gmail.com>
2022-12-21 17:56:28 -0800

    Exclude createdAt and updatedAt from login response

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -128,7 +128,7 @@ module.exports = (sequelize, DataTypes) => {
     scopes: {
       currentUser: {
         attributes: {
-          exclude: ['hashedPassword']
+          exclude: ['hashedPassword', 'createdAt', 'updatedAt']
         }
       },
       loginUser: {
