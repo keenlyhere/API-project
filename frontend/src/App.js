86891ade2a78cb8cba9777da1e9148380d9f0a7c
Keenly <keenlyhere@gmail.com>
2023-01-11 12:18:11 -0800

    Create Navigation component and connect to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import Navigation from "./components/Navigation";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
@@ -12,16 +13,23 @@ function App() {
     useEffect(() => {
         dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
     }, [dispatch])
-    return isLoaded && (
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-    );
+
+    return (
+      <div className="App-container">
+        <Navigation isLoaded={isLoaded} />
+            isLoaded && (
+                <Switch>
+                    <Route path="/login">
+                    <LoginFormPage />
+                    </Route>
+                    <Route path="/signup">
+                    <SignupFormPage />
+                    </Route>
+                </Switch>
+            );
+      </div>
+    )
+
 }
 
 export default App;
