c957b7920985b7d5f191f7d0b917fd0a800a4208
Keenly <keenlyhere@gmail.com>
2023-01-22 21:19:58 -0800

    Display spot not found if no spot found

--- frontend/src/components/Spots/SpotDetails/index.js
+++ frontend/src/components/Spots/SpotDetails/index.js
@@ -81,7 +81,11 @@ export default function SpotDetails() {
         }
     }
 
-    if (spot === undefined) return null;
+    if (spot === undefined) return (
+        <div className="SpotDetails-container">
+            <h1 className="SpotDetails-name">Spot Not Found</h1>
+        </div>
+    );
 
     if (user === undefined) return null;
 
